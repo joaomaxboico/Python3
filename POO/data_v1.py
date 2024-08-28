830cfec812fa708e6868d8e4750452270dba4fce
joaomaxboico <joo_max@yahoo.com.br>
2024-08-28 20:52:17 -0300

    POO-Método Construtor, parametros nomeados ou posicionais

--- POO/data_v1.py
+++ POO/data_v1.py
@@ -1,16 +1,21 @@
 class Data:
+    def __init__(self, dia=1, mes=11, ano=1970):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+    
     def __str__(self):
         return f'{self.dia}/{self.mes}/{self.ano}'
 
-d1 = Data()
-d1.dia = 5
-d1.mes = 12
-d1.ano = 2024
+d1 = Data(5, 12, 2019) #Método Construtor
+# d1.dia = 5
+# d1.mes = 12
+# d1.ano = 2024
 #print(f'{d1.dia}/{d1.mes}/{d1.ano}')
 print(d1)
 
-d2 = Data()
-d2.dia = 7
-d2.mes = 11
-d2.ano = 2025
+d2 = Data(ano=2024)
+d2.dia = 12
+# d2.mes = 11
+# d2.ano = 2025
 print(d2)
